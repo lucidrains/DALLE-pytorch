3a6afdebcb344ae8d73edf4054f507f2bc6f2b8f
Phil Wang <lucidrains@gmail.com>
2021-03-10 09:34:21 -0800

    fix bug with tensor slicing in sparse conv and axial attention

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'dalle-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.2.6',
+  version = '0.2.8',
   license='MIT',
   description = 'DALL-E - Pytorch',
   author = 'Phil Wang',
