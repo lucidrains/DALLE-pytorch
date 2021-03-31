bf54388ff28b0b579e174f13099fd9114d699480
ebert1 <ja.ebert@fz-juelich.de>
2021-03-31 14:32:57 +0200

    Prevent parallel checkpoint downloading

--- dalle_pytorch/vae.py
+++ dalle_pytorch/vae.py
@@ -15,10 +15,13 @@ from omegaconf import OmegaConf
 from taming.models.vqgan import VQModel
 
 import torch
+import torch.distributed
 from torch import nn
 import torch.nn.functional as F
 
 from einops import rearrange
+
+from dalle_pytorch import deepspeed_utils
 
 # constants
 
@@ -49,7 +52,8 @@ def unmap_pixels(x, eps = 0.1):
     return torch.clamp((x - eps) / (1 - 2 * eps), 0, 1)
 
 def download(url, filename = None, root = CACHE_PATH):
-    os.makedirs(root, exist_ok = True)
+    if deepspeed_utils.is_root_worker():
+        os.makedirs(root, exist_ok = True)
     filename = default(filename, os.path.basename(url))
 
     download_target = os.path.join(root, filename)
@@ -57,6 +61,10 @@ def download(url, filename = None, root = CACHE_PATH):
 
     if os.path.exists(download_target) and not os.path.isfile(download_target):
         raise RuntimeError(f"{download_target} exists and is not a regular file")
+
+    if not deepspeed_utils.is_root_worker() and not os.path.isfile(download_target):
+        # If the file doesn't exist yet, wait until it's downloaded by the root worker.
+        torch.distributed.barrier()
 
     if os.path.isfile(download_target):
         return download_target
@@ -72,6 +80,8 @@ def download(url, filename = None, root = CACHE_PATH):
                 loop.update(len(buffer))
 
     os.rename(download_target_tmp, download_target)
+    if deepspeed_utils.using_deepspeed and deepspeed_utils.is_root_worker():
+        torch.distributed.barrier()
     return download_target
 
 # pretrained Discrete VAE from OpenAI
