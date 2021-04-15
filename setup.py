4f0bb0013071a540876e72610aabd20244220373
Phil Wang <lucidrains@gmail.com>
2021-04-14 22:37:09 -0700

    add ability to use a huggingface tokenizer by specifying path to BPE json file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'dalle-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.9.6',
+  version = '0.10.0',
   license='MIT',
   description = 'DALL-E - Pytorch',
   author = 'Phil Wang',
@@ -24,6 +24,7 @@ setup(
     'pillow',
     'regex',
     'taming-transformers',
+    'tokenizers',
     'torch>=1.6',
     'torchvision',
     'tqdm'
