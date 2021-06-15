96a3286d03e4d1a8a792a1ae27b386d4b763851a
afiaka87 <3994972+afiaka87@users.noreply.github.com>
2021-06-14 20:27:08 -0500

    `--flops_profiler`, `--attn_dropout`, `--ff_dropout`

--- train_dalle.py
+++ train_dalle.py
@@ -434,9 +434,6 @@ for epoch in range(EPOCHS):
     if data_sampler:
         data_sampler.set_epoch(epoch)
     for i, (text, images) in enumerate(distr_dl):
-        if i == 201 and args.flops_profiler:
-            raise StopIteration("Profiler has finished running. Stopping training early.")
-
         if i % 10 == 0 and distr_backend.is_root_worker():
             t = time.time()
         if args.fp16:
@@ -494,6 +491,9 @@ for epoch in range(EPOCHS):
             sample_per_sec = BATCH_SIZE * 10 / (time.time() - t)
             log["sample_per_sec"] = sample_per_sec
             print(epoch, i, f'sample_per_sec - {sample_per_sec}')
+
+        if i == 201 and args.flops_profiler:
+            raise StopIteration("Profiler has finished running. Stopping training early.")
 
         if distr_backend.is_root_worker():
             wandb.log(log)
