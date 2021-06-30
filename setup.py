f8317f432ca9dafa4469e0cac6097e16b5bdc575
Phil Wang <lucidrains@gmail.com>
2021-06-30 10:15:52 -0700

    0.14.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'dalle-pytorch',
   packages = find_packages(),
   include_package_data = True,
-  version = '0.13.4',
+  version = '0.14.0',
   license='MIT',
   description = 'DALL-E - Pytorch',
   author = 'Phil Wang',
